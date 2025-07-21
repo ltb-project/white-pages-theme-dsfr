c24857dcf2a82b049bb8245224eb34eec665157a
Clément OUDOT <clement@oodo.net>
2025-07-21 15:53:55 +0200

    Check valid/invalid state and add CSS classes for error

--- htdocs/js/dsfr/dsfr.js
+++ htdocs/js/dsfr/dsfr.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
 
   // Change CSS classes of + and - buttons
   $('button[data-action=add]').on("click", function (event) {
-      $('button[data-action=del]').removeClass('btn-danger').removeClass('fr-icon-add-circle-line').addClass('fr-btn--secondary').addClass('fr-icon-delete-line');
+    $('button[data-action=del]').removeClass('btn-danger').removeClass('fr-icon-add-circle-line').addClass('fr-btn--secondary').addClass('fr-icon-delete-line');
+  });
+
+  // Catch valid / invalid state
+  $('.fr-input').bind('change', function (event) {
+    if ( $(event.target).is(':invalid') ) {
+      $(this).closest('.fr-input-group').addClass('fr-input-group--error');
+    } else {
+      $(this).closest('.fr-input-group').removeClass('fr-input-group--error');
+    }
   });
 
 });
